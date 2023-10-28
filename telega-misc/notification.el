06210bc8bdaedec201dc50a39a0273b676e6a8d3
Konstantin <alexandroviski8299@gmail.com>
2023-10-28 00:56:02 -0300

    Add files via upload

---
+++ telega-misc/notification.el
@@ -0,0 +1,15 @@
+(load-file "~/.emacs.d/telega-misc/alert.el")
+(load-file "~/.emacs.d/telega-misc/telega_alert.el")
+
+;; telega-alert-activation.el
+
+(setq alert-default-style 'libnotify)
+
+(defvar telega-alert-activation-enabled t)
+
+(defun activate-telega-alert-mode ()
+  (when (and telega-alert-activation-enabled
+             (string-match-p "telega" (buffer-name)))
+    (telega-alert-mode 1)))
+
+(add-hook 'buffer-list-update-hook 'activate-telega-alert-mode)
